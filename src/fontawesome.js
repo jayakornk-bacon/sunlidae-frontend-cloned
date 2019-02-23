42823b05712bb0326d44012157ff393a143b5c79
jayakornk <jayakornk@gmail.com>
2019-02-24 00:02:20 +0700

    🚚 split fontawesome into another file 🥓

---
+++ src/fontawesome.js
@@ -0,0 +1,8 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/pro-solid-svg-icons';
+import { far } from '@fortawesome/pro-regular-svg-icons';
+import { fal } from '@fortawesome/pro-light-svg-icons';
+import '@fortawesome/fontawesome-pro/css/all.min.css';
+
+library.add(fab, fas, far, fal);
